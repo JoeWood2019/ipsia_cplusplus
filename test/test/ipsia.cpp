35dca66eac473df71950a8d1053ce32ddf627138
camoomac <camoomac@163.com>
2015-11-26 18:14:31 +0800

    11_26

--- test/test/ipsia.cpp
+++ test/test/ipsia.cpp
@@ -111,7 +111,7 @@ double linearGM(Mat grad_mag, double xf, double yf, bool isHor)
 	}
 }
 
-bool trace_along_gradient(Mat grad_mag, int x,int y,double xf, double yf, double dy, double dx, bool isHor)
+bool trace_along_gradient(Mat grad_mag, int x,int y, double dy, double dx, bool isHor) // attention!! y= number of the current row, x= current col
 {
 	int cnt = 0;
 	double pre = grad_mag.at<double>(y, x);
@@ -119,8 +119,8 @@ bool trace_along_gradient(Mat grad_mag, int x,int y,double xf, double yf, double
 
 	while (true)
 	{
-		xf_next = xf + dx;
-		yf_next = yf + dy;
+		xf_next = x + dx;
+		yf_next = y + dy;
 		G = linearGM(grad_mag, xf_next, yf_next, true); // the gradient magnitude in (xf,yf) (a linear interpolation approximate) 
 		if (G == -1)
 		{
@@ -155,19 +155,18 @@ bool isEdge(Mat grad_mag, Mat img_grad_x, Mat img_grad_y, int y, int x) // atten
 		return flag;
 	}
 	// some preparation
-	double xf = x;
-	double yf = y;
 	double G = 0,diff =0;
-
 	double grad_x, grad_y;
 	grad_x = img_grad_x.at<double>(y, x);
 	grad_y = img_grad_y.at<double>(y, x);
+
+	// bi-trace from (y,x)
 	if (abs(grad_x) > abs(grad_y))
 	{
 		// trace along x positive
 		double dy = grad_y / grad_x;
 		double dx = 1;
-		flag = trace_along_gradient(grad_mag, x, y, xf, yf, dy, dx, true);
+		flag = trace_along_gradient(grad_mag, x, y, dy, dx, true);
 		if (!flag)
 		{
 			return false;
@@ -176,9 +175,7 @@ bool isEdge(Mat grad_mag, Mat img_grad_x, Mat img_grad_y, int y, int x) // atten
 		// trace along x negative
 		dx = -dx;
 		dy = -dy;
-		xf = x;
-		yf = y;
-		flag = trace_along_gradient(grad_mag, x, y, xf, yf, dy, dx, true);
+		flag = trace_along_gradient(grad_mag, x, y, dy, dx, true);
 		if (!flag)
 		{
 			return false;
@@ -186,14 +183,88 @@ bool isEdge(Mat grad_mag, Mat img_grad_x, Mat img_grad_y, int y, int x) // atten
 	}
 	else
 	{
+		// trace along y position
+		double dy = 1;
+		double dx = grad_x / grad_y;
+		flag = trace_along_gradient(grad_mag, x, y, dy, dx, true);
+		if (!flag)
+		{
+			return false;
+		}
+
+		// trace along x negative
+		dx = -dx;
+		dy = -dy;
+		flag = trace_along_gradient(grad_mag, x, y, dy, dx, true);
+		if (!flag)
+		{
+			return false;
+		}
 
 	}
 	return flag;
 }
 
-Mat img_grad_mask(Mat grad_mag,Mat grad_x,Mat grad_y, Mat img_sym, double TH_G, double TH_P)
+double min_8_neighbor(Mat image, int i, int j)// mininum in (x:x=2,y:y+2)
 {
-	if (grad_mag.depth() != 6)
+	if (image.depth() != CV_8U)
+	{
+		cout << "the data type of the image is wrong!" << endl;
+		exit(-1);
+	}
+	double neighbor[8];
+	neighbor[0] = image.at<uchar>(i, j);
+	neighbor[1] = image.at<uchar>(i, j + 1);
+	neighbor[2] = image.at<uchar>(i, j + 2);
+	neighbor[3] = image.at<uchar>(i + 1, j);
+	neighbor[4] = image.at<uchar>(i + 1, j + 2);
+	neighbor[5] = image.at<uchar>(i + 2, j);
+	neighbor[6] = image.at<uchar>(i + 2, j + 1);
+	neighbor[7] = image.at<uchar>(i + 2, j + 2);
+
+	double min_value;
+	min_value = neighbor[0];
+	for (int x = 1; x < 8; x++)
+	{
+		if (neighbor[x] < min_value)
+		{
+			min_value = neighbor[x];
+		}
+	}
+	return min_value;
+}
+double max_8_neighbor(Mat image, int i, int j) // maxinum in (x:x+2,y:y+2)
+{
+	if (image.depth() != CV_8U)
+	{
+		cout << "the data type of the image is wrong!" << endl;
+		exit(-1);
+	}
+	double neighbor[8];
+	neighbor[0] = image.at<uchar>(i, j);
+	neighbor[1] = image.at<uchar>(i, j + 1);
+	neighbor[2] = image.at<uchar>(i, j + 2);
+	neighbor[3] = image.at<uchar>(i + 1, j);
+	neighbor[4] = image.at<uchar>(i + 1, j + 2);
+	neighbor[5] = image.at<uchar>(i + 2, j);
+	neighbor[6] = image.at<uchar>(i + 2, j + 1);
+	neighbor[7] = image.at<uchar>(i + 2, j + 2);
+
+	double max_value;
+	max_value = neighbor[0];
+	for (int x = 1; x < 8; x++)
+	{
+		if (neighbor[x] > max_value)
+		{
+			max_value = neighbor[x];
+		}
+	}
+	return max_value;
+}
+
+Mat img_grad_mask(Mat grad_mag,Mat grad_x,Mat grad_y, Mat img_sym, double TH_G, double TH_P) // edge extraction
+{
+	if (grad_mag.depth() != CV_64F)
 	{
 		cout << "the data type of the grad_mag is wrong!" << endl;
 		exit(-1);
@@ -201,6 +272,8 @@ Mat img_grad_mask(Mat grad_mag,Mat grad_x,Mat grad_y, Mat img_sym, double TH_G, 
 	Mat mask;
 	mask.create(grad_mag.rows, grad_mag.cols, CV_8U); // 
 	int i = 0, j = 0;
+	bool flag_edge;
+
 	for (i = 0; i < grad_mag.rows; i++)
 	{
 		for (j = 0; j < grad_mag.cols; j++)
@@ -209,6 +282,17 @@ Mat img_grad_mask(Mat grad_mag,Mat grad_x,Mat grad_y, Mat img_sym, double TH_G, 
 			if (grad_mag.at<double>(i, j) < TH_G)
 			{
 				continue;
+			}
+			flag_edge = isEdge(grad_mag, grad_x, grad_y, i, j);
+			if (flag_edge)
+			{
+				double min_neighbor = min_8_neighbor(img_sym, i, j);
+				double max_neighbor = max_8_neighbor(img_sym, i, j);
+				double current_pixel = img_sym.at<uchar>(i, j);
+				if ( ((current_pixel - max_neighbor) > TH_P) || ((current_pixel - min_neighbor) < -TH_P) )
+				{
+					continue;
+				}
 			}
 			mask.at<unsigned char>(i, j) = 255;
 		}
@@ -263,25 +347,31 @@ Mat img_ipsia(string filename)
 	//Mat grad_angle;
 	//atan2(grad_y, grad_x, grad_angle);
 
-	Mat _img_dest;
-	double ostu_threh = threshold(img_input, _img_dest, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);// get the ostu threshold for canny
-	Mat img_canny;
-	Canny(img_input, img_canny, ostu_threh*0.5, ostu_threh, 3);
-	// use a mask to delete the isolated pixels
-	Mat kern_clean = (Mat_<char>(3, 3) << 1, 1, 1, 1, 0, 1, 1, 1, 1);
-	Mat img_canny_clean_mask;
-	filter2D(img_canny, img_canny_clean_mask, CV_16U, kern_clean); // after filter, the isolated pixel will be 0
-	img_canny_clean_mask = Mat_<uchar>(abs(grad_y));
-	Mat img_canny_clean; // delete isolated pixel and get a clean canny image
-	img_canny.copyTo(img_canny_clean, img_canny_clean_mask);
+	//Mat _img_dest;
+	//double ostu_threh = threshold(img_input, _img_dest, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);// get the ostu threshold for canny
+	//Mat img_canny;
+	//Canny(img_input, img_canny, ostu_threh*0.5, ostu_threh, 3);
+	//// use a mask to delete the isolated pixels
+	//Mat kern_clean = (Mat_<char>(3, 3) << 1, 1, 1, 1, 0, 1, 1, 1, 1);
+	//Mat img_canny_clean_mask;
+	//filter2D(img_canny, img_canny_clean_mask, CV_16U, kern_clean); // after filter, the isolated pixel will be 0
+	//img_canny_clean_mask = Mat_<uchar>(abs(grad_y));
+	//Mat img_canny_clean; // delete isolated pixel and get a clean canny image
+	//img_canny.copyTo(img_canny_clean, img_canny_clean_mask);
 
 	double TH_G = 100;
 	double TH_P = 50;
 	Mat img_mask = img_grad_mask(grad_magnitude, grad_x, grad_y, img_input_sym, TH_G, TH_P);
+	Mat kern_clean = (Mat_<char>(3, 3) << 1, 1, 1, 1, 0, 1, 1, 1, 1);
+	Mat img_mask_clean_temp;
+	filter2D(img_mask, img_mask_clean_temp, CV_8U, kern_clean); // after filter, the isolated pixel will be 0
+	Mat img_mask_clean; // delete isolated pixel and get a clean canny image
+	img_mask.copyTo(img_mask_clean, img_mask_clean_temp);
+
 	//Mat img_show;
-	//normalize(grad_magnitude, img_show, 0, 1, NORM_MINMAX);
-	imshow("gray_sobel_mag", img_mask);
+	//normalize(img_mask_clean, img_show, 0, 255, NORM_MINMAX);
+	imshow("gray_sobel_mag", img_mask_clean);
 	waitKey();
 
 	return img_H;
-}
+} 
--- test/test/ipsia.h
+++ test/test/ipsia.h
@@ -12,7 +12,9 @@ Mat img_gray_RGB2YCbCr(Mat src);
 Mat img_grad_mask(Mat grad_mag,Mat grad_x,Mat grad_y,Mat img_sym, double TH_G, double TH_P);
 bool isEdge(Mat grad_mag, Mat img_grad_x, Mat img_grad_y, int x, int y);
 double linearGM(Mat grad_mag,double xf,double yf,bool isHor);
-bool trace_along_gradient(Mat grad_mag, int x, int y, double xf, double yf, double dy, double dx, bool isHor)
+bool trace_along_gradient(Mat grad_mag, int x, int y, double dy, double dx, bool isHor);
+double min_8_neighbor(Mat image, int i, int j); // mininum in (x:x=2,y:y+2)
+double max_8_neighbor(Mat image, int i, int j); // maxnum in (x:x+2,y:y+2)
 
 Mat img_ipsia(string filename);
 #endif
